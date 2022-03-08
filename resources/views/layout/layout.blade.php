3190fc627ff92043612b4a2673af17b808446e83
Eliotboutet <bouteteliot@gmail.com>
2022-03-08 19:58:55 +0100

    initial commit

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,13 @@
+<html lang="fr">
+<head>
+    <title>@yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div>
+    @yield('content')
+</div>
+</body>
+</html>
